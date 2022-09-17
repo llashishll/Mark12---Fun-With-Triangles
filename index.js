cd00a8351ea0bc11b988a6c0a4df0fd5cd966a30
llashishll <ashishkalra999@gmail.com>
2022-09-17 07:14:27 +0530

    Updated CSS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const angl1 = document.querySelector('#angle1');
 const angl2 = document.querySelector('#angle2');
 const angl3 = document.querySelector('#angle3');
 const submit_btn = document.querySelector("#submit");
-const output1=document.querySelector("#output")
+const output1 = document.querySelector("#output")
 
 
 
@@ -10,11 +10,13 @@ function verifyForm() {
 
     var sumOfAngles = (Number(angl1.value) + Number(angl2.value) + Number(angl3.value))
     console.log(sumOfAngles);
-    if(sumOfAngles === 180 ){
-        output1.innerText="Yay! Your angles form a triangle. "
-        }else{
-        output1.innerText= "Oh! It can be anything but a triangle"
-            }
+    if (sumOfAngles === 180) {
+        output1.innerText = "Yay! Your angles form a triangle. "
+    } else if (sumOfAngles === 0) {
+        output1.innerText = "Please enter values of all 3 angles."
+    } else {
+        output1.innerText = "Oh! It can be anything but a triangle"
+    }
 
 }
 
